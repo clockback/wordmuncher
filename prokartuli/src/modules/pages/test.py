cf06db9655035dfdd25ba11d15b5de3d170a625c
Elliot Paton-Simpson <tuxbert>
2020-04-16 17:59:10 +1200

    Fixing another import error.

--- prokartuli/src/modules/pages/test.py
+++ prokartuli/src/modules/pages/test.py
@@ -3,7 +3,7 @@ from typing import Dict, Tuple
 
 from ...application import app
 from ..sheets import get_sheets, max_rows
-from prokartuli.modules.sql_handler import (
+from ..sql_handler import (
     get_connection, get_recent_translations
 )
 
