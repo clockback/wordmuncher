bb04a68743d73c8ad31126c1028135e42920815a
Elliot Paton-Simpson <tuxbert>
2020-04-16 17:49:08 +1200

    Fixing import issue.

--- prokartuli/src/main.py
+++ prokartuli/src/main.py
@@ -1,6 +1,6 @@
 __version__ = '0.1.0'
 
-from prokartuli.application import app
+from .application import app
 # noinspection PyUnresolvedReferences
 import prokartuli.modules.misc
 # noinspection PyUnresolvedReferences
