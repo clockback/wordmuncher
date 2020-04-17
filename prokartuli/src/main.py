44cf7def1dc54d5726e389de3f0996323a5049c6
Elliot Paton-Simpson <tuxbert>
2020-04-17 14:36:29 +1200

    Using waitress for server.

--- prokartuli/src/main.py
+++ prokartuli/src/main.py
@@ -3,24 +3,19 @@ __version__ = '0.1.0'
 from prokartuli.src.application import app
 # noinspection PyUnresolvedReferences
 from prokartuli.src.modules import misc
-
 # noinspection PyUnresolvedReferences
 from prokartuli.src.modules.pages import (
     customize, languages, mainpage, results, test, test_sheet
 )
-from logging import getLogger, ERROR
+from waitress import serve
 
-
-# Suppresses superfluous output.
-log = getLogger('werkzeug')
-log.setLevel(ERROR)
 
 # Contains settings
 max_sheets = 5
 
 
 def main():
-    app.run('prokartuli', debug=True)
+    serve(app, host='0.0.0.0', port=8080)
 
 
 if __name__ == '__main__':
