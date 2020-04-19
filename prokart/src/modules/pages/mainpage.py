05ef0e8f41a92df36f39480d1bef61ae424a6730
Elliot Paton-Simpson <tuxbert>
2020-04-19 20:34:36 +1200

    More documentation, specifically for mainpage.py.

--- prokart/src/modules/pages/mainpage.py
+++ prokart/src/modules/pages/mainpage.py
@@ -1,10 +1,18 @@
+# Builtins
+from typing import Tuple
+
+# Installed packages
 from flask import render_template
 
-
+# Local imports
 from prokart.src.application import app
 from prokart.src.modules.sql_handler import get_recent_translations
 
 
 @app.route('/')
-def main():
-    return render_template("main.html", topbar=get_recent_translations())
+def main() -> Tuple[str, int]:
+    """Returns the page for the main menu.
+    :return: The main menu page.
+    :rtype: Tuple[str, int]
+    """
+    return render_template("main.html", topbar=get_recent_translations()), 204
