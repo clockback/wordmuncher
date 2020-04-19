8a09a90a753d2e5b8e335d96605458863408c631
Elliot Paton-Simpson <tuxbert>
2020-04-20 10:45:01 +1200

    Finishing go_to function. Id can now be specified.

--- prokart/unit_tests/unit_tests.py
+++ prokart/unit_tests/unit_tests.py
@@ -15,15 +15,19 @@ from prokart.src.application import app
 
 
 class FirefoxDriver(wd.Firefox):
-    def go_to(self, location: str) -> None:
+    def go_to(
+            self, location: str, check_id: str = "sidebar-left-home"
+    ) -> None:
         """Goes to the provided location in Prokart
         :param str location: The location starting with '/'.
+        :param str check_id: The id of the element which indicates that
+            the page has finished loading.
         :return: None
         """
         self.get(f"http://0.0.0.0:8080{location}")
         try:
             WebDriverWait(self, 10).until(ec.presence_of_all_elements_located((
-                By.ID, 'sidebar-left-home'
+                By.ID, check_id
             )))
         except TimeoutException:
             raise TimeoutError("Page did not load.")
@@ -55,7 +59,6 @@ class BasicTests(unittest.TestCase):
         :return: None
         """
         self.driver.go_to('/')
-        sleep(1)
         sidebar = self.driver.find_element_by_css_selector(
             '.sidebar-center>button'
         )
