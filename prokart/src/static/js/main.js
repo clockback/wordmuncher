3aabf6da1aa50c31ac1cbf1381a57e41b1443924
Elliot Paton-Simpson <elliot@p-s.co.nz>
2021-10-10 13:51:35 +1300

    Main menu now uses module correctly.

---
+++ prokart/src/static/js/main.js
@@ -0,0 +1,15 @@
+import {getById} from './utils.js'
+import {prepareEventsTopBar} from './select_translator.js'
+
+function prepareEventsMain() {
+    getById("test-button").onclick = function () {
+        window.location.href = "/test";
+    };
+    getById("create-button").onclick = function () {
+        window.location.href = "/create";
+    };
+
+    prepareEventsTopBar();
+}
+
+window.addEventListener('load', prepareEventsMain);
