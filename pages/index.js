9302744fb6b34a77f9b76a83e08f580e97196f59
Ilya Simpson <elliot@p-s.co.nz>
2024-09-20 16:15:02 +1200

    Creating draft main page.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ repo }) {
     return (
         <div className={styles.container}>
             <Head>
@@ -9,9 +9,21 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main>
-                <h1 className={styles.title}>Word Muncher</h1>
-            </main>
+            <div className={styles.sidebar}>
+                <div>
+                    <h1 className={styles.title}>Word Muncher</h1>
+                    <a
+                        className={styles.inline}
+                        href="https://github.com/clockback/wordmuncher"
+                    >
+                        Contributions welcome!
+                    </a>
+                </div>
+            </div>
+            <div className={styles.vertical}>
+                <button className={styles.centre}>Learn</button>
+                <button className={styles.centre}>Vocab</button>
+            </div>
         </div>
     );
 }
