cd4b9e183e0c4f326b491dc2eb8a852163bee54a
Ilya Simpson <elliot@p-s.co.nz>
2024-09-23 15:45:56 +1200

    Increasing the quantity of HTML in the top-level layout.

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "#root/styles/global.css";
+import styles from "#root/styles/Home.module.css";
+
 import { Space_Grotesk } from "next/font/google";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
@@ -11,7 +13,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={spaceGrotesk.className}>{children}</body>
+            <body className={spaceGrotesk.className}>
+                <div className={styles.container}>
+                    <div className={styles.sidebar}>
+                        <div>
+                            <h1 className={styles.title}>Word Muncher</h1>
+                            <a
+                                className={styles.inline}
+                                href="https://github.com/clockback/wordmuncher"
+                            >
+                                Contributions welcome!
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className={styles.mainpage}>{children}</div>
+                </div>
+            </body>
         </html>
     );
 }
--- app/page.js
+++ app/page.js
@@ -2,28 +2,12 @@ import styles from "#root/styles/Home.module.css";
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <div className={styles.sidebar}>
-                <div>
-                    <h1 className={styles.title}>Word Muncher</h1>
-                    <a
-                        className={styles.inline}
-                        href="https://github.com/clockback/wordmuncher"
-                    >
-                        Contributions welcome!
-                    </a>
-                </div>
+        <div className={styles.vertical}>
+            <div className={styles.buttonpadding}>
+                <button className={styles.centre}>Learn</button>
             </div>
-
-            <div className={styles.mainpage}>
-                <div className={styles.vertical}>
-                    <div className={styles.buttonpadding}>
-                        <button className={styles.centre}>Learn</button>
-                    </div>
-                    <div className={styles.buttonpadding}>
-                        <button className={styles.centre}>Vocab</button>
-                    </div>
-                </div>
+            <div className={styles.buttonpadding}>
+                <button className={styles.centre}>Vocab</button>
             </div>
         </div>
     );
