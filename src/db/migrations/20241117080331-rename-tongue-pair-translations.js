18c0d6a9867065cc7f0126283f9280acadff1d4e
Ilya Simpson <elliot@p-s.co.nz>
2024-11-21 17:43:43 +1300

    Fixing migration needing transaction.

--- src/db/migrations/20241117080331-rename-tongue-pair-translations.js
+++ src/db/migrations/20241117080331-rename-tongue-pair-translations.js
@@ -3,28 +3,32 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.renameColumn(
-            "TonguePairs",
-            "translateFrom",
-            "translateFromTongueId",
-        );
-        await queryInterface.renameColumn(
-            "TonguePairs",
-            "translateTo",
-            "translateToTongueId",
-        );
+        await queryInterface.sequelize.transaction(async (t) => {
+            await queryInterface.renameColumn(
+                "TonguePairs",
+                "translateFrom",
+                "translateFromTongueId",
+            );
+            await queryInterface.renameColumn(
+                "TonguePairs",
+                "translateTo",
+                "translateToTongueId",
+            );
+        });
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.renameColumn(
-            "TonguePairs",
-            "translateFromTongueId",
-            "translateFrom",
-        );
-        await queryInterface.renameColumn(
-            "TonguePairs",
-            "translateToTongueId",
-            "translateTo",
-        );
+        await queryInterface.sequelize.transaction(async (t) => {
+            await queryInterface.renameColumn(
+                "TonguePairs",
+                "translateFromTongueId",
+                "translateFrom",
+            );
+            await queryInterface.renameColumn(
+                "TonguePairs",
+                "translateToTongueId",
+                "translateTo",
+            );
+        });
     },
 };
