7230856af96d62afc7129544e25d26ab7c4817b4
Ilya Simpson <elliot@p-s.co.nz>
2024-11-25 21:41:27 +1300

    Fixing models/associations for questions and sheets.

--- src/db/models/sheetquestion.ts
+++ src/db/models/sheetquestion.ts
@@ -1,33 +1,60 @@
-import { DataTypes, Model } from "sequelize";
+import {
+    CreationOptional,
+    DataTypes,
+    ForeignKey,
+    InferAttributes,
+    InferCreationAttributes,
+    Model,
+    NonAttribute,
+} from "sequelize";
 import sequelize from "./index";
+import Sheet from "./sheet";
+import Question from "./question";
+import TonguePair from "./tonguepair";
 
-class SheetQuestion extends Model {
-    declare sheet: number;
-    declare question: number;
+class SheetQuestion extends Model<
+    InferAttributes<SheetQuestion, { omit: "sheet" | "question" }>,
+    InferCreationAttributes<SheetQuestion, { omit: "sheet" | "question" }>
+> {
+    declare id: CreationOptional<number>;
+
+    declare sheetId: ForeignKey<Sheet["id"]>;
+    declare sheet: NonAttribute<Sheet>;
+
+    declare questionId: ForeignKey<Question["id"]>;
+    declare question: NonAttribute<Question>;
+
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
 }
 
 SheetQuestion.init(
     {
-        sheet: {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        sheetId: {
             type: DataTypes.INTEGER,
             references: {
                 model: "Sheets",
-                key: "id",
             },
             allowNull: false,
             onDelete: "CASCADE",
             onUpdate: "CASCADE",
         },
-        question: {
+        questionId: {
             type: DataTypes.INTEGER,
             references: {
-                model: "Sheets",
-                key: "id",
+                model: "Questions",
             },
             allowNull: false,
             onDelete: "CASCADE",
             onUpdate: "CASCADE",
         },
+        createdAt: DataTypes.DATE,
+        updatedAt: DataTypes.DATE,
     },
     {
         sequelize,
@@ -35,5 +62,40 @@ SheetQuestion.init(
         timestamps: true,
     },
 );
+
+SheetQuestion.belongsTo(Sheet, {
+    foreignKey: "sheetId",
+});
+
+SheetQuestion.belongsTo(Question, {
+    foreignKey: "questionId",
+});
+
+Question.hasMany(SheetQuestion, {
+    foreignKey: "questionId",
+});
+
+Question.belongsTo(TonguePair, {
+    foreignKey: "tonguePairId",
+});
+
+Question.belongsToMany(Sheet, {
+    through: SheetQuestion,
+    foreignKey: "sheetId",
+    as: "questionId",
+});
+Sheet.hasMany(SheetQuestion, {
+    foreignKey: "sheetId",
+});
+
+Sheet.belongsTo(TonguePair, {
+    foreignKey: "tonguePairId",
+});
+
+Sheet.belongsToMany(Question, {
+    through: SheetQuestion,
+    foreignKey: "questionId",
+    as: "sheetId",
+});
 
 export default SheetQuestion;
