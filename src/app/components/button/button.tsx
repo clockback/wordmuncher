d7325cad8be6b848ab7663a71aeea4f361fa5c98
Ilya Simpson <elliot@p-s.co.nz>
2024-11-29 13:48:31 +1300

    Adding tongue selector page.

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -3,11 +3,16 @@ import styles from "./button.module.css";
 interface ButtonProps {
     children: React.ReactNode;
     href?: string | null;
+    onClick?: () => void;
 }
 
-export default function Button({ children, href }: ButtonProps) {
+export default function Button({ children, href, onClick }: ButtonProps) {
     if (href === null || href === undefined) {
-        return <button className={styles.centre}>{children}</button>;
+        return (
+            <button className={styles.centre} onClick={onClick}>
+                {children}
+            </button>
+        );
     } else {
         return (
             <a className={styles.centre} href={href}>
