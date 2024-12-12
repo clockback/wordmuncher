5292bb67029e1c4797ec22421b681bcb7a2f99d3
Ilya Simpson <elliot@p-s.co.nz>
2024-12-12 20:28:57 +1300

    Adding seed for settings in dev.

---
+++ src/db/seeders/dev-only/20241212063000-insert-settings.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert("Settings", [
+            {
+                tonguePairId: 1,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.delete("Settings", null);
+    },
+};
