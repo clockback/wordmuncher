a427eba65bd25576286001e86c8feac1bdde4558
Ilya Simpson <elliot@p-s.co.nz>
2024-12-23 06:26:34 +1300

    Allow deletion of questions.

---
+++ src/app/vocab/delete-question/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { Question } from "@models";
+
+export async function DELETE(request: NextRequest) {
+    const requestJSON = await request.json();
+    const questionId = requestJSON.id;
+
+    let noDeleted = await Question.destroy({ where: { id: questionId } });
+
+    if (noDeleted === 0) {
+        return NextResponse.json(
+            { error: `Sheet does not exist with id ${questionId}` },
+            { status: 409 },
+        );
+    }
+
+    return NextResponse.json({}, { status: 200 });
+}
