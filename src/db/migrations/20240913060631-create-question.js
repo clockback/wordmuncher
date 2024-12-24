ac4047efa185069998c49ecc6e0c5c147b9388d2
Ilya Simpson <elliot@p-s.co.nz>
2024-12-24 17:17:56 +1300

    Obtaining Sequelize via require in migrations.

--- src/db/migrations/20240913060631-create-question.js
+++ src/db/migrations/20240913060631-create-question.js
@@ -1,7 +1,10 @@
 "use strict";
+
+const Sequelize = require("sequelize");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
+    async up(queryInterface) {
         await queryInterface.createTable("Questions", {
             id: {
                 allowNull: false,
@@ -33,7 +36,7 @@ module.exports = {
             },
         });
     },
-    async down(queryInterface, Sequelize) {
+    async down(queryInterface) {
         await queryInterface.dropTable("Questions");
     },
 };
--- src/db/seeders/dev-only/20241211170925-insert-answers.js
+++ src/db/seeders/dev-only/20241211170925-insert-answers.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
+    async up(queryInterface) {
         await queryInterface.bulkInsert("Answers", [
             {
                 questionId: 1,
@@ -28,7 +28,7 @@ module.exports = {
         ]);
     },
 
-    async down(queryInterface, Sequelize) {
+    async down(queryInterface) {
         await queryInterface.bulkDelete("Answers", null);
     },
 };
