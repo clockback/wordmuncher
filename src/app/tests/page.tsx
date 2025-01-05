a79d1066c2bb203169923ef3b378457b7726fdb0
Ilya Simpson <elliot@p-s.co.nz>
2025-01-05 16:47:17 +1300

    Display sheets for testing.

---
+++ src/app/tests/page.tsx
@@ -0,0 +1,82 @@
+import { Question, Result, Sheet, SheetQuestion } from "@models";
+
+import styles from "./tests.module.css";
+import { getSettings } from "src/db/helpers/settings";
+
+async function getAllSheets(): Promise<Sheet[]> {
+    "use server";
+    const settings = await getSettings();
+
+    return await Sheet.findAll({
+        where: { tonguePairId: settings.tonguePairId },
+        include: [
+            {
+                model: Question,
+                as: "questions",
+                include: [{ model: Result, as: "result" }],
+            },
+        ],
+    });
+}
+
+async function getSheetsByProgress(): Promise<
+    { id: number; sheetName: string; progress: number }[]
+> {
+    "use server";
+    const sheetsWithoutProgress = await getAllSheets();
+    const sheets = [];
+    for (let sheet of sheetsWithoutProgress) {
+        let progressTotal = 0;
+        let questionCount = sheet.questions.length;
+        console.log(questionCount);
+        for (let question of sheet.questions) {
+            const result = question.result;
+            if (result === null) {
+                continue;
+            }
+            const isCompleted = result.current == result.goal;
+            const currentStars = result.stars + Number(isCompleted);
+            const targetStars = result.stars + 1;
+            progressTotal += currentStars / targetStars;
+        }
+
+        const progress = questionCount ? progressTotal / questionCount : 0;
+
+        sheets.push({
+            id: sheet.id,
+            sheetName: sheet.sheetName,
+            progress: progress,
+        });
+    }
+    return sheets;
+}
+
+export default async function Tests() {
+    const allSheets = await getSheetsByProgress();
+
+    const tableRows = [];
+
+    for (let sheet of allSheets) {
+        tableRows.push(
+            <tr key={sheet.id}>
+                <td>{sheet.sheetName}</td>
+                <td>{sheet.progress * 100}%</td>
+            </tr>,
+        );
+    }
+
+    return (
+        <div className={styles.centre}>
+            <h1>Select a sheet</h1>
+            <table className={styles.allsheets}>
+                <thead>
+                    <tr>
+                        <th>Sheet name</th>
+                        <th>Progress</th>
+                    </tr>
+                </thead>
+                <tbody>{tableRows}</tbody>
+            </table>
+        </div>
+    );
+}
