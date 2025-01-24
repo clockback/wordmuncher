aaf46d04612f6a84513bc3b06a0532f1da53b191
Ilya Simpson <elliot@p-s.co.nz>
2025-01-25 10:59:40 +1300

    Showing results at the end of a test.

--- src/app/tests/[sheet]/context.ts
+++ src/app/tests/[sheet]/context.ts
@@ -12,16 +12,22 @@ interface TestSheetContextType {
     pending: boolean;
     question: Question;
     questionNumber: number;
+    numberCorrect: number;
+    numberIncorrect: number;
     setCurrentAnswer: Dispatch<SetStateAction<string>>;
     setExpectedAnswer: Dispatch<SetStateAction<Answer | null>>;
     setLastQuestions: Dispatch<SetStateAction<number[]>>;
     setNextQuestion: Dispatch<SetStateAction<Question>>;
+    setNumberCorrect: Dispatch<SetStateAction<number>>;
+    setNumberIncorrect: Dispatch<SetStateAction<number>>;
     setPending: Dispatch<SetStateAction<boolean>>;
     setQuestion: Dispatch<SetStateAction<Question>>;
     setQuestionNumber: Dispatch<SetStateAction<number>>;
     setShowMessageToFinish: Dispatch<SetStateAction<boolean>>;
+    setShowResults: Dispatch<SetStateAction<boolean>>;
     sheet: Sheet;
     showMessageToFinish: boolean;
+    showResults: boolean;
 }
 
 const testSheetContext = createContext<TestSheetContextType | undefined>(
