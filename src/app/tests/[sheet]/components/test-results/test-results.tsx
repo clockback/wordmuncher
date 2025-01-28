1d09fd57246f58341a4318c4abdb076c4211f471
Ilya Simpson <elliot@p-s.co.nz>
2025-01-28 17:19:52 +1300

    Showing the number of stars concluding a test in a prettier fashion.

--- src/app/tests/[sheet]/components/test-results/test-results.tsx
+++ src/app/tests/[sheet]/components/test-results/test-results.tsx
@@ -1,6 +1,9 @@
+import clsx from "clsx";
 import { useContext } from "react";
 
 import testSheetContext from "../../context";
+import Star from "../star/star";
+import styles from "./test-results.module.css";
 
 export default function TestResults() {
     const {
@@ -10,15 +13,46 @@ export default function TestResults() {
         numberOfStars,
     } = useContext(testSheetContext);
     const totalQuestions = numberCorrect + numberIncorrect;
+
+    const changeStars = numberOfStars - startingNumberOfStars;
+    let changeStarsText = `+${changeStars}`;
+    if (changeStars < 0) {
+        changeStarsText = `-${-changeStars}`;
+    }
+    const changeStarsStyle = clsx(styles.numberofstars, {
+        [styles.gainstars]: changeStars > 0,
+        [styles.losestars]: changeStars < 0,
+    });
+
     return (
         <>
+            <div className={styles.starpadding}>
+                <div className={styles.starinfo}>
+                    <div className={styles.starslabel}>Stars before</div>
+                    <div>
+                        <span className={styles.numberofstars}>
+                            {startingNumberOfStars}
+                        </span>
+                        <Star></Star>
+                    </div>
+                </div>
+                <div className={styles.starinfo}>
+                    <div className={styles.starslabel}>Stars after</div>
+                    <div>
+                        <span className={styles.numberofstars}>
+                            {numberOfStars}
+                        </span>
+                        <Star></Star>
+                    </div>
+                </div>
+                <div>
+                    <span className={changeStarsStyle}>{changeStarsText}</span>
+                    <Star></Star>
+                </div>
+            </div>
             <div>
                 You answered {numberCorrect}/{totalQuestions} questions
                 correctly!
-            </div>
-            <div>
-                You had {startingNumberOfStars} stars, but now you have{" "}
-                {numberOfStars} stars!
             </div>
         </>
     );
