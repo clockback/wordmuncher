f27f8e3086a9fd42e5144b634fd202d3e00ad4d1
Ilya Simpson <elliot@p-s.co.nz>
2025-02-16 22:02:56 +1300

    Fixing mislabeling while transitioning to define-category.

--- src/app/components/define-category/define-category.tsx
+++ src/app/components/define-category/define-category.tsx
@@ -24,7 +24,7 @@ function featuresIncludeName(
     return false;
 }
 
-export default function AddCategory({
+export default function DefineCategory({
     category,
     setCategory,
     features,
