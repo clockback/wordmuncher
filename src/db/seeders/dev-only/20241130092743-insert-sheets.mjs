f9146da947944d2ad5d1222fd33877848ac1d751
Ilya Simpson <elliot@p-s.co.nz>
2025-02-18 17:56:00 +1300

    Reorganizing and combining seeders.

--- src/db/seeders/dev-only/20241130092743-insert-sheets.mjs
+++ src/db/seeders/dev-only/20241130092743-insert-sheets.mjs
@@ -21,6 +21,12 @@ export async function up({ context: queryInterface }) {
             createdAt: new Date(),
             updatedAt: new Date(),
         },
+        {
+            sheetName: "Sheet 4",
+            tonguePairId: 1,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
     ]);
 }
 export async function down({ context: queryInterface }) {
