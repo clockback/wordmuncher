01d4270651c284ac52b1d3a4b5ff3d800063571a
Ilya Simpson <elliot@p-s.co.nz>
2025-03-03 22:49:09 +1300

    Separating production environment (specifically the database path).

--- src/tests/database.setup.ts
+++ src/tests/database.setup.ts
@@ -1,17 +1,18 @@
 import { test as setup } from "@playwright/test";
 import { unlink } from "fs/promises";
 
+import { pathToDB } from "../db/config/config.mjs";
 import { devSeeder, migrator, seeder } from "../db/umzug";
 
 setup("Set up database", async ({}) => {
     try {
-        await unlink("/tmp/wordmuncher.sqlite");
+        await unlink(pathToDB);
         console.log("Deleted existing database file.");
     } catch {
         console.log("No database file to delete.");
     }
 
-    console.info("Creating new database!");
+    console.info(`Creating new database at "${pathToDB}".`);
     await migrator.up();
     await seeder.up();
     await devSeeder.up();
