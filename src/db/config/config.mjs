14f4326ffc6a92deb0f723710749daa4ac5c6d85
Ilya Simpson <elliot@p-s.co.nz>
2025-03-03 22:49:09 +1300

    Separating production environment (specifically the database path).

--- src/db/config/config.mjs
+++ src/db/config/config.mjs
@@ -1,13 +1,25 @@
-export const options = {
-    dialect: "sqlite",
-    storage: "/tmp/wordmuncher.sqlite",
-    logging: false,
-};
+import fs from "fs";
+import os from "os";
+import path from "path";
 
-const allOptions = {
-    development: options,
-    test: options,
-    production: options,
-};
+const homedir = os.homedir();
+export const pathToDataDir = path.join(
+    homedir,
+    ".local",
+    "share",
+    "wordmuncher",
+);
 
-export default allOptions;
+if (process.env.NODE_ENV == "production" && !fs.existsSync(pathToDataDir)) {
+    fs.mkdirSync(pathToDataDir, { recursive: true });
+}
+
+export let pathToDB;
+if (process.env.NODE_ENV == "production") {
+    pathToDB = path.join(pathToDataDir, "wordmuncher.sqlite");
+} else {
+    pathToDB = "/tmp/wordmuncher.sqlite";
+}
+
+const options = { dialect: "sqlite", logging: false, storage: pathToDB };
+export default options;
--- src/db/models/db-connection.ts
+++ src/db/models/db-connection.ts
@@ -1,10 +1,8 @@
 import { Sequelize } from "sequelize";
 import { SequelizeOptions } from "sequelize-typescript";
 
-import { options } from "../config/config.mjs";
+import options from "../config/config.mjs";
 
-const dbOptions = options;
-
-const sequelize = new Sequelize(dbOptions as SequelizeOptions);
+const sequelize = new Sequelize(options as SequelizeOptions);
 
 export default sequelize;
